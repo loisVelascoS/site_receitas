606128a0cb8d8f0b2c48f55e5c83fd2a942f3d67
Lóis <112522486+loisVelascoS@users.noreply.github.com>
2023-03-08 17:57:52 -0300

    att

--- keeprecipe.sql
+++ keeprecipe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 27-Fev-2023 às 08:39
+-- Tempo de geração: 02-Mar-2023 às 07:54
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.1.1
 
@@ -24,28 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `ingredientes_receita`
---
-
-CREATE TABLE `ingredientes_receita` (
-  `id_receita` int(11) NOT NULL,
-  `cod_ingrediente` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `ingredients`
---
-
-CREATE TABLE `ingredients` (
-  `id` int(11) NOT NULL,
-  `ingredient` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Estrutura da tabela `recipe`
 --
 
@@ -53,16 +31,43 @@ CREATE TABLE `recipe` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `modo_preparo` varchar(255) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `id_user` int(11) NOT NULL
+  `ingredient` varchar(255) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `recipe`
 --
 
-INSERT INTO `recipe` (`id`, `title`, `modo_preparo`, `created_at`, `id_user`) VALUES
-(1, 'Miojo', 'Por no microondas', '2023-02-27 04:31:13', 0);
+INSERT INTO `recipe` (`id`, `title`, `modo_preparo`, `ingredient`, `id_user`, `created_at`) VALUES
+(8, 'Brownie', 'Misture os ovos e o açúcar.  Em seguida, agregue todos os outros ingredientes até formar um creme uniforme.  Despeje em uma assadeira, forrada com papel-manteiga e leve ao forno médio por 40 minutos.  O brownie estará pronto quando a parte de cima estiver', '6 colheres (sopa) bem cheias, de margarina sem sal 3/4 xícara (chá) achocolatado 1/2 xícara (chá) chocolate em pó 1 e 1/4 xícara (chá) farinha de trigo 2 xícaras (chá) açúcar 4 ovos 2 pitadas de sal 1 colheres (chá) de extrato ou essência de baunilha 1 ta', 4, '2023-03-02 03:31:27'),
+(9, 'Pudim', '1 Numa forma para pudim, de 20 centímetros de diâmetro, coloque 6 colheres de sopa de açúcar e leve ao fogo médio até virar uma calda caramelada, por mais ou menos 3 minutos. 2 Retire do fogo e vá virando a fôrma, de modo que a calda forre todo o fundo e ', '6 colheres de sopa de açúcar 1 lata de leite condensado 1 lata de leite, use a mesma medida do leite condensado 3 ovos', 5, '2023-03-02 03:33:45'),
+(12, 'Miojo', 'Por no microondas', 'agua e miojo', 5, '2023-03-02 03:47:22');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `scores`
+--
+
+CREATE TABLE `scores` (
+  `id` int(11) NOT NULL,
+  `score` int(11) NOT NULL,
+  `coment` varchar(255) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_recipe` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `scores`
+--
+
+INSERT INTO `scores` (`id`, `score`, `coment`, `id_user`, `id_recipe`, `created_at`) VALUES
+(6, 10, 'Muito deliciosa', 4, 8, '2023-03-02 03:31:59'),
+(7, 10, 'Amei muito', 5, 9, '2023-03-02 03:36:18'),
+(10, 8, 'bom mas não é saudável', 5, 12, '2023-03-02 03:47:44');
 
 -- --------------------------------------------------------
 
@@ -82,30 +87,27 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `created_at`) VALUES
-(1, 'loisv', '$2y$10$vhtL25nIoYFpJn35nnpYi.EwNJMyE45bhBB9x3atnrSAj6hAAEOCS', '0000-00-00 00:00:00'),
-(2, 'test', '$2y$10$r3bfQXQ9vGsnXAamQlhxyOa9nxo71D0WA2VzvYi9CnMwJ.6d8Pr9u', '0000-00-00 00:00:00');
+(4, 'loisv', '$2y$10$kDR0J0QOXqNDXKxwdEpCKeacWXqOiZzkac77im.DnI7QrrOeC0FVu', '0000-00-00 00:00:00'),
+(5, 'test', '$2y$10$WIA8Oh1F8LpPr5vGjLEWieAQlfjBYl1cINQwLYTlgSh0tbS8zl8wO', '0000-00-00 00:00:00');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices para tabela `ingredientes_receita`
---
-ALTER TABLE `ingredientes_receita`
-  ADD PRIMARY KEY (`id_receita`,`cod_ingrediente`);
-
---
--- Índices para tabela `ingredients`
---
-ALTER TABLE `ingredients`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Índices para tabela `recipe`
 --
 ALTER TABLE `recipe`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_recipe_users` (`id_user`);
+
+--
+-- Índices para tabela `scores`
+--
+ALTER TABLE `scores`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_scores_recipe` (`id_recipe`),
+  ADD KEY `fk_scores_users` (`id_user`);
 
 --
 -- Índices para tabela `users`
@@ -118,22 +120,39 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT de tabela `ingredients`
---
-ALTER TABLE `ingredients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT de tabela `recipe`
 --
 ALTER TABLE `recipe`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT de tabela `scores`
+--
+ALTER TABLE `scores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `recipe`
+--
+ALTER TABLE `recipe`
+  ADD CONSTRAINT `fk_recipe_users` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Limitadores para a tabela `scores`
+--
+ALTER TABLE `scores`
+  ADD CONSTRAINT `fk_scores_recipe` FOREIGN KEY (`id_recipe`) REFERENCES `recipe` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_scores_users` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
